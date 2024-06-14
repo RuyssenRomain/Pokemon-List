5a55d5ed5f806597e3a91b80e6cfa768ae140f7a
Ruyssen Romain <ruyssensud@gmail.com>
2024-06-14 15:40:00 +0200

    integrate function update and delete in pokemon-form files

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -1,27 +1,44 @@
 import Pokemon from "../models/pokemon";
- 
+
 export default class PokemonService {
- 
+
   static getPokemons(): Promise<Pokemon[]> {
     return fetch('http://localhost:3001/pokemons')
       .then(response => response.json())
       .catch(error => this.handleError(error));
   }
- 
-  static getPokemon(id: number): Promise<Pokemon|null> {
+
+  static getPokemon(id: number): Promise<Pokemon | null> {
     return fetch(`http://localhost:3001/pokemons/${id}`)
       .then(response => response.json())
       .then((data => this.isEmpty(data) ? null : data))
       .catch(error => this.handleError(error));
-      
+
   }
- 
+  static updatePokemon(pokemon: Pokemon): Promise<Pokemon> {
+    return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(pokemon),
+      headers: { 'content-Type': 'application/json' }
+    })
+      .then(response => response.json())
+      .catch(error => this.handleError(error));
+  }
+  static deletePokmemon(pokemon: Pokemon): Promise<{}> {
+    return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
+      method: 'DELETE',
+      headers: { 'content-type': 'application/json' }
+    })
+    .then(response => response.json())
+    .catch(error => this.handleError(error));
+  }
+
   static isEmpty(data: Object): boolean {
     return Object.keys(data).length === 0;
   }
-  
-  static handleError(error:Error):void{
-      console.error(error);
+
+  static handleError(error: Error): void {
+    console.error(error);
   }
 
 }
